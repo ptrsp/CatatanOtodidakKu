14629b0e69f138e12b5cde31f88dfcead3502454
peiterspasaribu <peiterpasaribu@gmail.com>
2022-12-18 10:19:47 +0700

    Merapikan Peletakan File dan Folder

---
+++ Python/SamplePythonProgram/subcode_inherimport.py
@@ -0,0 +1,14 @@
+class Identity:
+    # fn="Last, FirstName"
+    # tp="Title Paper"
+    # pe="Lembaga Penerbit"
+    def __init__(self,fin,lan,tp,pe):
+        print("Data Yang Anda Masukan : ")
+        print(fin+" "+lan+" - "+tp+" - "+pe)
+        self.fn=lan+". "+fin
+        self.tp="\""+tp+"\""
+        self.pe="copyright@ "+pe
+class Reference(Identity):
+    def wReference(self, number):
+        print("Format Referensi : ")
+        print("["+number+"]"+self.fn+", "+self.tp+", "+self.pe)
