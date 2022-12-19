fe199ccab1a13b34a94298bb7db12004c5a8caa3
peiterspasaribu <peiterpasaribu@gmail.com>
2022-12-19 12:41:22 +0700

    Mengimprove Perulangan Input Folder, eks Memastikan Direktori Exist Memastikan Format Ekstensi sesuai tanpa adanya spasi (hanya . diikuti huruf ataupun angka)

--- Python/SamplePythonProgram/RenameFile.py
+++ Python/SamplePythonProgram/RenameFile.py
@@ -10,24 +10,38 @@ if __name__=="__main__":
         FtargetDir=FtargetDir.PathMatcher()
         if(FtargetDir=="False"):
             print("Format Direktori Tidak Sesuai, Harap Ulangi!")
-    statEks=False
-    while(statEks==False):
-        print("Format Ekstensi [Contoh: .jpg atau .yml atau .py atau lainnya")
-        eks=input("Masukkan Ekstensi Target = ")
-        if(re.search(r"^\.",eks)):
-            statEks=True
         else:
-            print("Format Ekstensi Tidak Sesuai!")
-
+            if(os.path.exists(FtargetDir)==False):
+                FtargetDir="False"
+                print("Direktori Tidak Ditemukan, Harap Ulangi!")
+    statEksO=False
+    statEksN=False
+    while(statEksO==False or statEksN==False):
+        print("Format Ekstensi [Contoh: .jpg atau .yml atau .py atau lainnya")
+        eksO=input("Masukkan Ekstensi Target = ")
+        eksN=input("Masukkan Ekstensi Baru = ")
+        if(re.search(r"^\.[a-zA-Z0-9]+",eksO)):
+            statEksO=True
+        if(re.search(r"^\.[a-zA-Z0-9]+",eksN)):
+            statEksN=True
+        if(statEksO==False):
+            print("Format Ekstensi Target Tidak Sesuai!")
+        if(statEksN==False):
+            print("Format Ekstensi Baru Tidak Sesuai!")
+    statRename=False
     for (root,dirs,files) in os.walk(FtargetDir, topdown=True):
         for file in files:
             instRM = RM.RegexMatcher(file)
-            stateFile = instRM.extMatcher(eks)
+            stateFile = instRM.extMatcher(eksO)
             sBool,indeks=stateFile
             if(sBool==True):
                 oldName=file
                 file=file[0:indeks]
-                os.rename(os.path.join(root, oldName), os.path.join(root, file+".yaml"))
+                os.rename(os.path.join(root, oldName), os.path.join(root, file+eksN))
                 print("Mengubah Nama : ")
-                print(oldName+" Menjadi "+file)
-    print("Proses Pengubahan Nama Selesai!")
+                print(oldName+" Menjadi "+file+eksN)
+                statRename=True
+    if(statRename==False):
+        print("Tidak Ada File Yang Berubah!")
+    else:
+        print("Proses Mengubah Nama Selesai!")
